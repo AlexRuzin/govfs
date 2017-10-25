dbaf991f978e0b36cc5de88e29bbbcebd98e43dd
AlexRuzin <stan.ruzin@gmail.com>
2017-10-25 12:54:07 -0400

    Removed some junk functions used to test gob encoder/decoder

--- govfs_test.go
+++ govfs_test.go
@@ -30,97 +30,10 @@ import (
     _"bytes"
     "bytes"
     "runtime"
-    "encoding/gob"
-	"log"
-	_"fmt"
-    "fmt"
 )
 
 const FS_DATABASE_FILE string = "test_db"
 
-
-func TestGOBEncoderSample(t *testing.T) {
-	type P struct {
-	X, Y, Z int
-	Name    string
-	}
-
-	type Q struct {
-	X, Y *int32
-	Name string
-	}
-
-
-	// Initialize the encoder and decoder. Normally enc and dec would be
-	// bound to network connections and the encoder and decoder would
-	// run in different processes.
-	var network bytes.Buffer        // Stand-in for a network connection
-	enc := gob.NewEncoder(&network) // Will write to network.
-	dec := gob.NewDecoder(&network) // Will read from network.
-
-	// Encode (send) some values.
-	err := enc.Encode(P{3, 4, 5, "Pythagoras"})
-	if err != nil {
-		log.Fatal("encode error:", err)
-	}
-	err = enc.Encode(P{1782, 1841, 1922, "Treehouse"})
-	if err != nil {
-		log.Fatal("encode error:", err)
-	}
-
-	// Decode (receive) and print the values.
-	var q P
-	err = dec.Decode(&q)
-	if err != nil {
-		log.Fatal("decode error 1:", err)
-	}
-	fmt.Printf("%q: {%d, %d}\n", q.Name, q.X, q.Y)
-	err = dec.Decode(&q)
-	if err != nil {
-		log.Fatal("decode error 2:", err)
-	}
-	fmt.Printf("%q: {%d, %d}\n", q.Name, q.X, q.Y)
-
-	// Output:
-	// "Pythagoras": {3, 4}
-	// "Treehouse": {1782, 1841}
-}
-
-func TestGobSerializer(t *testing.T) {
-    type st struct {
-        val1 string
-        val2 int
-        val3 []byte
-    }
-
-    input := st{
-        val1: "test1",
-        val2: 4343,
-    }
-    input.val3 = make([]byte, 4)
-    input.val3 = []byte{1, 2, 3, 4 }
-
-    serialized_fileheader := func (p st) *bytes.Buffer {
-        b := bytes.Buffer{}
-        e := gob.NewEncoder(&b)
-        e.Encode(p)
-        return &b
-    } (input) /* Pass in RawFile */
-
-    output, _ := func (p *bytes.Buffer) (*st, error) {
-        output := &st{}
-
-        d := gob.NewDecoder(p)
-        err := d.Decode(output)
-        if err != nil && err != io.EOF {
-            return nil, err
-        }
-
-        return output, nil
-    } (serialized_fileheader)
-
-    out(output.val1)
-}
 
 func TestFSWriter(t *testing.T) {
     /*
