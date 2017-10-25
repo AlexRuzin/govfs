c9efacb4c10eaa65f4f5f9821e7a5bd1a9a0bfa4
AlexRuzin <stan.ruzin@gmail.com>
2017-10-24 22:54:33 -0400

    Added test gob encoder/decoder

--- govfs_test.go
+++ govfs_test.go
@@ -31,9 +31,59 @@ import (
     "bytes"
     "runtime"
     "encoding/gob"
+	"log"
+	"fmt"
 )
 
 const FS_DATABASE_FILE string = "test_db"
+
+
+func TestGOBEncoderSample(t *testing.T) {
+	type P struct {
+	X, Y, Z int
+	Name    string
+	}
+
+	type Q struct {
+	X, Y *int32
+	Name string
+	}
+
+
+	// Initialize the encoder and decoder. Normally enc and dec would be
+	// bound to network connections and the encoder and decoder would
+	// run in different processes.
+	var network bytes.Buffer        // Stand-in for a network connection
+	enc := gob.NewEncoder(&network) // Will write to network.
+	dec := gob.NewDecoder(&network) // Will read from network.
+
+	// Encode (send) some values.
+	err := enc.Encode(P{3, 4, 5, "Pythagoras"})
+	if err != nil {
+		log.Fatal("encode error:", err)
+	}
+	err = enc.Encode(P{1782, 1841, 1922, "Treehouse"})
+	if err != nil {
+		log.Fatal("encode error:", err)
+	}
+
+	// Decode (receive) and print the values.
+	var q Q
+	err = dec.Decode(&q)
+	if err != nil {
+		log.Fatal("decode error 1:", err)
+	}
+	fmt.Printf("%q: {%d, %d}\n", q.Name, *q.X, *q.Y)
+	err = dec.Decode(&q)
+	if err != nil {
+		log.Fatal("decode error 2:", err)
+	}
+	fmt.Printf("%q: {%d, %d}\n", q.Name, *q.X, *q.Y)
+
+	// Output:
+	// "Pythagoras": {3, 4}
+	// "Treehouse": {1782, 1841}
+}
 
 func TestGobSerializer(t *testing.T) {
     type st struct {
